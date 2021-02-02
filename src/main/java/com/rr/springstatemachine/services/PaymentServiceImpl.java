f3d1343ebd398552f61b08d286b47bf881d0f2fa
Ravi Ravula <rr@test.com>
2021-02-02 17:29:19 -0500

    Initialize State Machine from the state saved in DB. closes #7

--- src/main/java/com/rr/springstatemachine/services/PaymentServiceImpl.java
+++ src/main/java/com/rr/springstatemachine/services/PaymentServiceImpl.java
@@ -7,6 +7,7 @@ import com.rr.springstatemachine.repository.PaymentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
@@ -24,16 +25,41 @@ public class PaymentServiceImpl implements PaymentService {
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
     }
 
     @Override
     public StateMachine<PaymentState, PaymentEvent> declinePayment(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
         return null;
+    }
+
+    private StateMachine<PaymentState, PaymentEvent> build(Long paymentId) {
+        // get payment from db
+        Payment payment = paymentRepository.getOne(paymentId);
+
+        // get state machine instance
+        StateMachine<PaymentState, PaymentEvent> sm = stateMachineFactory.getStateMachine(Long.toString(payment.getId()));
+
+        // stop it
+        sm.stop();
+
+        // reset state machine state
+        sm.getStateMachineAccessor()
+                .doWithAllRegions(sma -> {
+                    sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
+                });
+
+        // start it
+        sm.start();
+
+        return sm;
     }
 }
