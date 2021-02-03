18227584b20f2d7cf36a0615ab4e86f029dc0202
Ravi Ravula <rr@test.com>
2021-02-03 11:07:26 -0500

    Implement state change intercetor. closes #9

--- src/main/java/com/rr/springstatemachine/services/PaymentServiceImpl.java
+++ src/main/java/com/rr/springstatemachine/services/PaymentServiceImpl.java
@@ -20,6 +20,7 @@ public class PaymentServiceImpl implements PaymentService {
 
     private final PaymentRepository paymentRepository;
     private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+    private final PaymentStateChangeInterceptor paymentStateChangeInterceptor;
 
     @Override
     public Payment newPayment(Payment payment) {
@@ -72,6 +73,7 @@ public class PaymentServiceImpl implements PaymentService {
         // restore state machine state (not reset as in previous commit)
         sm.getStateMachineAccessor()
                 .doWithAllRegions(sma -> {
+                    sma.addStateMachineInterceptor(paymentStateChangeInterceptor);
                     sma.resetStateMachine(new DefaultStateMachineContext<>(payment.getState(), null, null, null));
                 });
 
